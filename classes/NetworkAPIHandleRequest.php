ede0cf9cdd718a4cd38f903c2ecf957c80da0831
billchetch <45006134+billchetch@users.noreply.github.com>
2026-01-31 22:22:39 +0700

    asdf

--- classes/NetworkAPIHandleRequest.php
+++ classes/NetworkAPIHandleRequest.php
@@ -163,7 +163,7 @@ class NetworkAPIHandleRequest extends chetch\api\APIHandleRequest{
 
 			case 'remote-host':
 				if(empty($payload['remote_host_name']))throw new Exception("Cannot update remote host as no remote_host_name provided");
-				//if(!isset($payload['request_open']))throw new Exception("Cannot update remote host as no request_open provided");
+				if(!isset($payload['request_open']))throw new Exception("Cannot update remote host as no request_open provided");
 				/*unset($payload['id']);
 				if($payload['request_open']){
 					$payload['opened_on'] = now();
