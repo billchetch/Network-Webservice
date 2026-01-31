65daa5aff77753dc3b871ad691fd379e7821d965
billchetch <45006134+billchetch@users.noreply.github.com>
2026-01-31 22:48:54 +0700

    asdf

--- classes/NetworkAPIHandleRequest.php
+++ classes/NetworkAPIHandleRequest.php
@@ -173,7 +173,7 @@ class NetworkAPIHandleRequest extends chetch\api\APIHandleRequest{
 				} else {
 					$payload['last_checked'] = self::now(false);
 				}
-				$payload['wan_ip'] = $_REQUEST['REMOTE_HOST'];
+				$payload['wan_ip'] = $_SERVER['REMOTE_ADDR'];
 				$host = RemoteHost::createInstance($payload);
 				$host->write(true);
 				$data = $host->getRowData();
