0dd95c8a342219ae86622b1dfb76518067543332
billchetch <45006134+billchetch@users.noreply.github.com>
2026-02-01 18:56:17 +0700

    asdf

---
+++ get_remote_host.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This script calls puts to a webservice to request open or close a ssh tunnel to a remote host.
+ * See network_update.php for explanation on how remote hosts work.
+ */
+
+require_once('_include.php');
+
+use chetch\Config as Config;
+use chetch\sys\Logger as Logger;
+use chetch\sys\SysInfo as SysInfo;
+use chetch\network\Network as Network;
+use chetch\Utils as Utils;
+use chetch\api\APIMakeRequest as APIMakeRequest;
+
+$log = null;
+try{
+	$lf = "\n";
+	$log = Logger::getLog('get remote host', Logger::LOG_TO_SCREEN);
+    $log->start();
+
+	if($argc < 2)throw new Exception("Please supply a remote host name an argument to this script");
+	$remoteHostName = $argv[1];
+	
+	//Retreive remote-host data from webservice
+	$apiBaseURL = Config::get('REMOTE_API_BASE_URL', "http://network.bulan-baru.com:8001/api/");
+	$log->info("Requesting remote-host $remoteHostName from $apiBaseURL...");
+	$requestParams = array('remote_host_name' => $remoteHostName);
+	$req = APIMakeRequest::createGetRequest($apiBaseURL, 'remote-host', $requestParams);
+	try{
+		$remoteHostData = $req->request(); //this will throw if not found
+		if($remoteHostData['request_open'] && !empty($remoteHostData['opened_on']) && empty($remoteHostData['closed_on'])){
+			$log->info("$remoteHostName is open, opened on ".$remoteHostData['opened_on']);
+		} else if($remoteHostData['request_open'] && empty($remoteHostData['opened_on'])){
+			$log->info("$remoteHostName waiting to open, updated on ".$remoteHostData['last_updated']);
+		} else if(!empty($remoteHostData['opened_on']) && !empty($remoteHostData['closed_on'])){
+			$log->info("$remoteHostName is closed");
+		} else if(!$remoteHostData['request_open'] && empty($remoteHostData['closed_on'])){
+			$log->info("$remoteHostName waiting to close");
+		}
+	} catch (Exception $e){
+		$log->warning("$remoteHostName NOT found on server!");
+		throw $e;
+	}
+
+	$log->finish();
+
+} catch (Exception $e){
+	if($log){
+		$log->exception($e->getMessage());
+        	$log->info("get remote host exited because of exception: ".$e->getMessage());
+	} else {
+		echo "EXCEPTION: ".$e->getMessage();
+	}
+}
+
+?>
