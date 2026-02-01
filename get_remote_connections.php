15506771c43ab6bf2a09712d7f8bc85f2d107e92
billchetch <45006134+billchetch@users.noreply.github.com>
2026-02-02 01:02:04 +0700

    asdf

--- get_remote_connections.php
+++ get_remote_connections.php
@@ -33,15 +33,14 @@ try{
 
 		foreach($connections as $cnn){
 			$connection = $cnn['connection'];
-			$log->info("------- $connection connection -------");
 			if($cnn['request_open'] && !empty($cnn['opened_on']) && empty($cnn['closed_on'])){
-				$log->info("$connection is OPEN, opened on ".$cnn['opened_on']);
+				$log->info("----- $connection is OPEN, opened on ".$cnn['opened_on']);
 			} else if($cnn['request_open'] && empty($cnn['opened_on'])){
-				$log->info("$connection waiting to open, updated on ".$cnn['last_updated']);
+				$log->info("----- $connection waiting to open, updated on ".$cnn['last_updated']);
 			} else if(!empty($cnn['opened_on']) && !empty($cnn['closed_on'])){
-				$log->info("$connection is CLOSED");
+				$log->info("----- $connection is CLOSED");
 			} else if(!$cnn['request_open'] && empty($cnn['closed_on'])){
-				$log->info("$connection waiting to close");
+				$log->info("----- $connection waiting to close");
 			}
 		}
 	} catch (Exception $e){
