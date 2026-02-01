0f328dbb2c57c7ff1575f0313caea0138e7810e2
billchetch <45006134+billchetch@users.noreply.github.com>
2026-02-01 10:00:22 +0700

    asdf

--- classes/NetworkAPIHandleRequest.php
+++ classes/NetworkAPIHandleRequest.php
@@ -191,9 +191,9 @@ class NetworkAPIHandleRequest extends chetch\api\APIHandleRequest{
 				$hostname = $payload['remote_host_name'];
 				$request2open = $payload['request_open'];
 				$host = RemoteHost::getByHostName($hostname);
-				/*if($host->get('request_open') == $request2open){
-					throw new Exception("Request to ".($request2open ? 'open' : 'close')." already PUT");
-				}*/
+				if($host->get('request_open') == $request2open){
+					//throw new Exception("Request to ".($request2open ? 'open' : 'close')." already PUT");
+				}
 
 				$payload['last_updated'] = self::now(false);
 				if($payload['request_open']){ //If requesting to open we reset previous date info
